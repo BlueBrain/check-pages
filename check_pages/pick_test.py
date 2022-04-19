43f75a26bb92a83e71275921e689119f690df58c
adietz <alexander.dietz@epfl.ch>
2022-04-19 13:19:51 +0200

    Committing first draft of pick-neuron test

---
+++ check_pages/pick_test.py
@@ -0,0 +1,71 @@
+"""Tests for 'pick-real-neuron'.
+"""
+from selenium.webdriver.common.by import By
+
+
+class PickNeuronTests:
+    """Defines the Testing class for 'pick-real-neuron'."""
+
+    # These URL lead to the simui pages.
+    URL = "https://bbp.epfl.ch/pick-real-neuron/"
+    TEXT_REAL = "that is a real neuron"
+    TEXT_SYNTH = "that is a synthesized neuron"
+
+    def __init__(self, driver):
+        """Initializes this test object with the seleniumbase-seleniumwire webdriver."""
+        # The driver
+        self.driver = driver
+
+    def open(self):
+        """Open the main page."""
+        self.driver.open(self.URL)
+
+    def click_image(self, correct=True):
+        """Click on image and check outcome."""
+
+        # Do we want correct or incorrect guess?
+        if correct:
+            xpath = "(//img)[3]"
+            txt = self.TEXT_REAL
+        else:
+            xpath = "(//img)[4]"
+            txt = self.TEXT_SYNTH
+
+        # Check image properties
+        image = self.driver.find_element(xpath, by=By.XPATH)
+        assert image.get_attribute("src").endswith(".jpeg")
+        assert image.size["height"] == 750
+
+        # Click on image
+        self.driver.click(xpath)
+
+        # Check for correct overlay text
+        for _ in range(20):
+            if self.driver.is_text_visible(txt):
+                return
+        assert False
+
+    def check_score(self, score):
+        """Check the given 'score'."""
+        self.driver.find_element(f"//div[text()='{score}']", by=By.XPATH)
+
+    def perform_test(self):
+        """Performs the actual test by opening the page and clicking on two images."""
+        self.open()
+
+        # Click on correct image
+        self.click_image(True)
+        self.check_score(2)
+
+        # Click on incorrect image
+        self.click_image(False)
+        self.check_score(3)
+
+        # Quit the browser
+        self.driver.tearDown()
+
+
+def test_pickneuron(selbase):
+    """Wrapper for the tests."""
+    ebrains = PickNeuronTests(selbase)
+    ebrains.perform_test()
